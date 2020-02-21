845afed39e9fcff56b9f4f5f4fda387f7eef0ca8
juanc <juanc@LAPTOP-5J79I0R8.home>
2020-02-21 15:27:05 +0100

    primer commit en multiplicacion

---
+++ multiplicacion/PruebaMultiplicacion/src/main/java/multiplicacion.java
@@ -0,0 +1,16 @@
+
+import es.uned.master.java.PruebaMultiplicacion.*;
+public class multiplicacion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		PruebaMultiplicacion prueba = new PruebaMultiplicacion();
+		
+		prueba.setX(30);
+		prueba.setY(40);
+		
+		System.out.println(prueba);
+	}
+
+}
