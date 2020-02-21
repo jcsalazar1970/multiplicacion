19f09aed36d9f82d54b37ec90bee632ed78b1072
juanc <juanc@LAPTOP-5J79I0R8.home>
2020-02-21 20:18:26 +0100

    segundo commit sobre prueba multiplicacion

--- multiplicacion/PruebaMultiplicacion/src/main/java/Driver.java
+++ multiplicacion/PruebaMultiplicacion/src/main/java/Driver.java
@@ -1,6 +1,6 @@
 
 import es.uned.master.java.PruebaMultiplicacion.*;
-public class multiplicacion {
+public class Driver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
